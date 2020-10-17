3dabf239e2336457f8ab885d2a56bc9c22253b3b
PrahladKumar1316 <prahladkumar1316@gmail.com>
2020-10-17 23:31:57 +0530

    Graphs are changed to bar type

--- public/js/country-wise.js
+++ public/js/country-wise.js
@@ -55,14 +55,15 @@ const fun = async ()=>{
 
     const confirmedCases = document.getElementById('confirmedCases').getContext('2d');
     const myChart1 = new Chart(confirmedCases, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: dates,
             datasets: [{
                 label: 'Cofirmed',
                 data: confirmed,
+                hoverBackgroundColor: 'rgba(255, 7, 58, 1)',
                 backgroundColor: 'rgba(255, 7, 58, 0.345)',
-                borderColor: 'rgba(255, 7, 58, 1)',
+                borderColor: 'rgba(255, 7, 58, 0.5)',
                 borderWidth: 1
             }]
         },
@@ -88,14 +89,15 @@ const fun = async ()=>{
 
     const activeCases = document.getElementById('activeCases').getContext('2d');
     const myChart2 = new Chart(activeCases, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: dates,
             datasets: [{
                 label: 'Active',
                 data: active,
+                hoverBackgroundColor: 'rgba(0, 123, 255, 1)',
                 backgroundColor: 'rgba(0,123,255,.345)',
-                borderColor: '#007bff',
+                borderColor: 'rgba(0, 123, 255, 0.5)',
                 borderWidth: 1
             }]
         },
@@ -120,14 +122,15 @@ const fun = async ()=>{
 
     const recoveredCases = document.getElementById('recoveredCases').getContext('2d');
     const myChart3 = new Chart(recoveredCases, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: dates,
             datasets: [{
                 label: 'Recovered',
                 data: recovered,
+                hoverBackgroundColor: 'rgba(40, 167, 69, 1)',
                 backgroundColor: 'rgba(40,167,69,.345)',
-                borderColor: '#28a745',
+                borderColor: 'rgba(40, 167, 69, 0.5)',
                 borderWidth: 1
             }]
         },
@@ -152,14 +155,15 @@ const fun = async ()=>{
 
     const deceasedCases = document.getElementById('deceasedCases').getContext('2d');
     const myChart4 = new Chart(deceasedCases, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: dates,
             datasets: [{
                 label: 'Deceased',
                 data: deceased,
+                hoverBackgroundColor: 'rgba(108, 117, 125, 1)',
                 backgroundColor: 'rgba(108,117,125,.345)',
-                borderColor: '#6c757d',
+                borderColor: 'rgba(108, 117, 125, 0.5)',
                 borderWidth: 1
             }]
         },
@@ -184,14 +188,15 @@ const fun = async ()=>{
 
     const testedCases = document.getElementById('testedCases').getContext('2d');
     const myChart5 = new Chart(testedCases, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: dates,
             datasets: [{
                 label: 'Tested',
                 data: tested,
+                hoverBackgroundColor: 'rgba(150, 115, 185, 1)',
                 backgroundColor: 'rgba(150,115,185,.433333)',
-                borderColor: '#9673b9',
+                borderColor: 'rgba(150, 115, 185, 0.5)',
                 borderWidth: 1
             }]
         },
